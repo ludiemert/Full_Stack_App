1fbc517022d597ec65b030340f8c6dcd204ae2e9
ludiemert <lucianadiemert@gmail.com>
2023-11-15 19:50:54 -0300

    Seu primeiro commit na branch main

---
+++ front/src/App.js
@@ -0,0 +1,71 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+//router
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+//components
+import Write from "./pages/Write";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/post/:id",
+        element:<Single/>
+
+      },
+      {
+        path:"/write",
+        element:<Write/>
+      },
+    ]    
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+]);
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router}/>
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ front/src/pages/Single.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Single = () => {
+  return (
+    <div>
+      Single
+    </div>
+  )
+}
+
+export default Single
