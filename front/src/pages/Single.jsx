d443de812bc5637b5c94b672982edfaff5c432d9
ludiemert <lucianadiemert@gmail.com>
2023-11-21 20:37:45 -0300

    feat: add pages Single and Write and Components Menu

--- front/src/pages/Single.jsx
+++ front/src/pages/Single.jsx
@@ -1,12 +1,62 @@
 import React from "react";
-
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
-    <div>
-      Single
-    </div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img
+          src="https://images.pexels.com/photos/103123/pexels-photo-103123.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <img src={Delete} alt="" />
+          </div>
+        </div>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. </h1>
+        <p>
+          {" "}
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet
+          explicabo perspiciatis optio quia, ea hic autem omnis vero, expedita
+          nulla sit corporis pariatur doloribus debitis cumque nostrum harum aut
+          enim!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+          nisi nostrum cupiditate voluptatum tenetur libero adipisci delectus
+          veritatis sint, vel iste earum ipsa illo itaque voluptates repellat
+          sequi veniam ipsum. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Amet explicabo perspiciatis optio quia, ea hic autem
+          omnis vero, expedita nulla sit corporis pariatur doloribus debitis
+          cumque nostrum harum aut enim!Lorem ipsum dolor, sit amet consectetur
+          adipisicing elit. Fuga nisi nostrum cupiditate voluptatum tenetur
+          libero adipisci delectus veritatis sint, vel iste earum ipsa illo
+          itaque voluptates repellat sequi veniam ipsum. <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet
+          explicabo perspiciatis optio quia, ea hic autem omnis vero, expedita
+          nulla sit corporis pariatur doloribus debitis cumque nostrum harum aut
+          enim!Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga
+          nisi nostrum cupiditate voluptatum tenetur libero adipisci delectus
+          veritatis sint, vel iste earum ipsa illo itaque voluptates repellat
+          sequi veniam ipsum.
+        </p>
+      </div>
 
-export default Single
+      <Menu />
+    </div>
+  );
+};
+
+export default Single;
--- front/src/pages/Write.jsx
+++ front/src/pages/Write.jsx
@@ -1,9 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const Write = () => {
-  return (
-    <div>Write</div>
-  )
-}
+  const [value, setValue] = useState("");
 
-export default Write
+  console.log(value);
+
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+          <input style={{ display: "none" }} type="file" id="file" name="" />
+          <label className="file" htmlFor="file">Upload Image</label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+
+        <div className="item">
+          <h1>Category</h1>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="art" id="art" />
+          <label htmlFor="art">Art</label>
+          </div>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="science" id="science" />
+          <label htmlFor="science">Science</label>
+          </div>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="technology" id="technology" />
+          <label htmlFor="technology">Technology</label>
+          </div>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="cinema" id="cinema" />
+          <label htmlFor="cinema">Cinema</label>
+          </div>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="design" id="design" />
+          <label htmlFor="design">Design</label>
+          </div>
+
+          <div className="cat">
+          <input type="radio" name="cat" value="foot" id="foot" />
+          <label htmlFor="foot">Foot</label>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Write;
