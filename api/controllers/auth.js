215c8496588911245bb02cba9cf8086d5cd4dd9d
ludiemert <lucianadiemert@gmail.com>
2023-11-23 19:19:51 -0300

    feat: created controllers auth.js the username, email,password and library the password bcrypt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,30 @@
-export const register = (req,res)=>{
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
 
-}
+export const register = (req, res) => {
+  //check existing user
+  const q = "SELECT * FROM users WHERE email = ? or username = ?";
 
-export const login = (req,res)=>{
-  
-}
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.js(err);
+    if (data.length) return res.status(409).json("User already exists!!");
 
-export const logout = (req,res)=>{
-  
-}
+    //Hash the password and creta a user
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    // Store hash in your password DB.
+
+    const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username. req.body.email, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("User has been created")
+      //O usuário foi criado
+    });
+  });
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
