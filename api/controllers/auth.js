3dc342076cb70879dc2e40ddad94849cd586895b
ludiemert <lucianadiemert@gmail.com>
2023-11-23 18:56:22 -0300

    feat: add and configure route auth.js

---
+++ api/controllers/auth.js
@@ -0,0 +1,11 @@
+export const register = (req,res)=>{
+
+}
+
+export const login = (req,res)=>{
+  
+}
+
+export const logout = (req,res)=>{
+  
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
-import express from "express"
-import postRoutes from "./routes/posts.js"
-
-const app = express()
-
-app.use(express.json())
-app.use("/api/posts", postRoutes)
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 
-app.listen(8800,()=>{
-  console.log("Connected!!!!")
-})
+const app = express();
+
+app.use(express.json());
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.listen(8800, () => {
+  console.log("Connected!!!!");
+});
