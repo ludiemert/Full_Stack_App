8b4fb0b93ba608610a0f02bdbdcfeca80452df17
ludiemert <lucianadiemert@gmail.com>
2023-11-27 18:49:27 -0300

    feat:  made adjustments page  login

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,18 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+  const [err, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/auth/login", inputs);
+      navigate("/");
+    } catch (err) {
+      setError(err.response.data);
+    }
+  };
+
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="username" />
-        <input required type="email" placeholder="email" />
-        <input required type="password" placeholder="password" />
-        <button>Login</button>
-        <p>This is an error!</p>
+        <input required type="text" placeholder="username" onChange={handleChange} />
+        <input required type="password" placeholder="password" onChange={handleChange} />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p>{err}</p>}
         <span>Don't you have an account? <Link to="/register">Register</Link>
-
         </span>
       </form>
       
