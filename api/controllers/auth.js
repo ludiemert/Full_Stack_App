4a726b77f783f6e500c1aa66f51d9970e8b44382
ludiemert <lucianadiemert@gmail.com>
2023-11-27 19:12:04 -0300

    feat:  made adjustments controllers auth.js and add yarn jsonwebtoken (It serves to guarantee integrity and authenticity and is generated using the algorithm specified in the header and a secret key or a public/private key pair.)  token web Json

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -17,13 +17,28 @@ export const register = (req, res) => {
     const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
     const values = [req.body.username, req.body.email, hash];
 
-    db.query(q, [values], (err,data) => {
+    db.query(q, [values], (err, data) => {
       if (err) return res.json(err);
       return res.status(200).json("User has been created");
     });
   });
 };
 
-export const login = (req, res) => {};
+export const login = (req, res) => {
+  //CHECK USER
+
+  const q = "SELECT * FROM users WHERE username =?";
+
+  db.query(q, [req, body, username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length === 0) return res.status(404).json("User not found!!!");
+
+    //check password
+    const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+    if(!isPasswordCorrect) return res.status(400).json("Wrong username or password!!😣 Nome de usuário ou senha incorretos")
+  });
+};
+
+
 
 export const logout = (req, res) => {};
