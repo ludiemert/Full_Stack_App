ca3ac3fb66e780dc6850bd6d9c5311691e37cb23
ludiemert <lucianadiemert@gmail.com>
2023-11-28 20:03:48 -0300

    feat: configuration of the logout page, and photos of the login and logout page in a folder.

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -56,4 +56,12 @@ export const login = (req, res) => {
   });
 };
 
-export const logout = (req, res) => {};
+export const logout = (req, res) => {
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out🤩.");
+};
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Logo from "../img/logo.png"
-  
+import Logo from "../img/logo.png";
+import { AuthContext } from "../context/authContext";
+
 const Navbar = () => {
+  const { currentUser, logout } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -10,34 +13,42 @@ const Navbar = () => {
           <img src={Logo} alt="" />
         </div>
         <div className="links">
-          <Link className='link' to="/?cat=art">
+          <Link className="link" to="/?cat=art">
             <h6>ART</h6>
-            </Link>
-            <Link className='link' to="/?cat=art">
+          </Link>
+          <Link className="link" to="/?cat=art">
             <h6>SCIENCE</h6>
-            </Link>
-            <Link className='link' to="/?cat=art">
+          </Link>
+          <Link className="link" to="/?cat=art">
             <h6>TECHNOLOGY</h6>
-            </Link>
-            <Link className='link' to="/?cat=art">
+          </Link>
+          <Link className="link" to="/?cat=art">
             <h6>CINEMA</h6>
-            </Link>
-            <Link className='link' to="/?cat=art">
+          </Link>
+          <Link className="link" to="/?cat=art">
             <h6>DESIGN</h6>
-            </Link>
-            <Link className='link' to="/?cat=art">
+          </Link>
+          <Link className="link" to="/?cat=art">
             <h6>FOOD</h6>
-            </Link>
+          </Link>
 
-            <span>John</span>
-            <span>Logout</span>
-            <span className="write">
-              <Link className="link" to="/write">Write</Link>
-            </span>
-        </div>   
+          <span>{currentUser?.username}</span>
+          {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
