2581136a6f667aab5acf0dffa10817dd07ccff0c
ludiemert <lucianadiemert@gmail.com>
2023-11-28 19:26:45 -0300

    feat: login page authentication

--- front/src/index.js
+++ front/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.js';
 import App from './App';
+import { AuthContexProvider } from './context/authContext.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContexProvider>
     <App />
+    </AuthContexProvider>
   </React.StrictMode>
 );
--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,9 +1,9 @@
-import axios from "axios";
-import React, { useState } from "react";
+//import axios from "axios";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Login = () => {
-
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
@@ -13,6 +13,8 @@ const Login = () => {
 
   const navigate = useNavigate();
 
+  const { login } = useContext(AuthContext);
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -20,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("/auth/login", inputs);
+      await login(inputs)
       navigate("/");
     } catch (err) {
       setError(err.response.data);
@@ -28,19 +30,31 @@ const Login = () => {
   };
 
   return (
-    <div className='auth'>
+    <div className="auth">
       <h1>Login</h1>
       <form>
-        <input required type="text" placeholder="username" name="username" onChange={handleChange} />
-        <input required type="password" placeholder="password" name="password" onChange={handleChange} />
+        <input
+          required
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
         <button onClick={handleSubmit}>Login</button>
         {err && <p>{err}</p>}
-        <span>Don't you have an account? <Link to="/register">Register</Link>
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
         </span>
       </form>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
