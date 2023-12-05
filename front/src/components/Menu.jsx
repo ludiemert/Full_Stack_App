676e8d7fb8a3549026d482a96cc6290c74d9af46
ludiemert <lucianadiemert@gmail.com>
2023-12-04 21:06:20 -0300

    feat: configure final project adjustments, blog page upload tests

--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -54,7 +54,7 @@ const Menu = ({ cat }) => {
       <h1>Other posts you may like 😉</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <img src={post.img} alt="" />
+          <img src={`../upload/${post?.img}`} alt="" />
           <h2>{post.title}</h2>
           <button>Read More</button>
         </div>
--- front/src/pages/Single.jsx
+++ front/src/pages/Single.jsx
@@ -38,10 +38,15 @@ const Single = () => {
     }
   };
 
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
   return (
     <div className="single">
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
           {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
@@ -58,7 +63,7 @@ const Single = () => {
           )}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
+        {getText(post.desc)}
       </div>
       <Menu cat={post.cat}/>
     </div>
